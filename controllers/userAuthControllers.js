136b289a18af0fb7555cd8b7d5f95cf116217c4f
Edward Nyame <xbeeneski@gmail.com>
2025-04-25 23:07:19 +0000

    Fix to cookie headers

--- controllers/userAuthControllers.js
+++ controllers/userAuthControllers.js
@@ -78,8 +78,8 @@ export const register = async ( req, res ) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+      secure: true,
+      sameSite: "none",
       maxAge: 2* 24 * 60 * 60 * 1000
     })
 
@@ -133,10 +133,10 @@ export const login = async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
-      maxAge: 2 * 24 * 60 * 60 * 1000,
-    });
+      secure: true,
+      sameSite: "none",
+      maxAge: 2* 24 * 60 * 60 * 1000
+    })
 
     return res.json({
       success: true,
@@ -155,8 +155,8 @@ export const logout = async ( req, res ) => {
   try {
     res.clearCookie('token', {
       httpOnly: true,
-     secure: process.env.NODE_ENV === "production",
-     sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+     secure: true,
+     sameSite: "none",
      maxAge: 7 * 24 * 60 * 60 * 1000
     })
 
