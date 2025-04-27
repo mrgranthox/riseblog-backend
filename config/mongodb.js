f0bc2488e26bd87055cbecb16d256effa3e9482f
Edward Nyame <xbeeneski@gmail.com>
2025-04-27 00:21:08 +0000

    env modified 2

--- config/mongodb.js
+++ config/mongodb.js
@@ -4,7 +4,7 @@ import mongoose from 'mongoose'
 const connectDB = async () => {
   mongoose.connection.on("connected", () => console.log("Databse Connected"))
 
-  await mongoose.connect(`${process.env.MONGODB_URL}/blog-post`)
+  await mongoose.connect(`${process.env.MONGODB_URL_NEW }/blog-post`)
 }
 
 export default connectDB;
